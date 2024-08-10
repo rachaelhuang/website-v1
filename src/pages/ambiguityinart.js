40499b08ec8977be1b679d94966c68b8faac7753
Rachael <156548776+rachaelhuang@users.noreply.github.com>
2024-08-10 17:24:36 -0400

    added demo videos

--- src/pages/ambiguityinart.js
+++ src/pages/ambiguityinart.js
@@ -11,7 +11,7 @@ function AmbiguityInArt() {
           <p>Facilitating a neuroscience research study through visitors at the Hood Museum of Art.</p>
         </div>
       </div>
-      
+
       <section className="information">
         <div className="info-section">
           <p>Timeline</p>
@@ -46,10 +46,23 @@ function AmbiguityInArt() {
       </section>
 
       <section className="final-product-teaser">
-        <h2>Final Product Teaser</h2>
+        <h2>Product Demo</h2>
         <p>Here's a sneak peek of the final product. Scroll down to see the full case study.</p>
-        <video autoPlay loop muted className="case-study-video">
-          <source src="/assets/ambiguityinart/ambiguityinart_demovid1.mp4" type="video/mp4" /> </video>
+        
+        <div className="video-container">
+          <video autoPlay loop muted className="case-study-video">
+            <source src="/assets/ambiguityinart/ambiguityinart_demovid1.mov" type="video/mp4" />
+          </video>
+          <img src="/assets/ambiguityinart/iphone15frame.png" className="case-study-video-frame"/>
+        </div>
+
+        <div className="video-container">
+          <video autoPlay loop muted className="case-study-video">
+            <source src="/assets/ambiguityinart/ambiguityinart_demovid2.mov" type="video/mp4" />
+          </video>
+          <img src="/assets/ambiguityinart/iphone15frame.png" className="case-study-video-frame"/>
+        </div>
+
       </section>
 
       <section className="problem-statement">
@@ -58,43 +71,44 @@ function AmbiguityInArt() {
       </section>
 
       <section className="project-purpose">
-      <h3>Partner Requests</h3>
-      <p>Because this app's primary purpose is to collect research data from Hood Museum-goers, the FINN Lab partners presented to us their idea of the app's general flow that would best inform their research goals.</p>
-      <p>Add visual of this flow?</p>
-      <p>As such, we made sure to adhere to our partner's research goals and preferred flow during our ideation and design process.</p>
-        </section>
-        
-        <section className="user-research">
-      <h3>User interviews</h3>
-            <p>We brainstormed interview questions and conducted interviews for students and faculty around campus that had varying degrees of interest in museum going and/or relevant backgrounds in art. The goal of our user interviews was to gauge the following:</p>
-              <li>What is the typical user's experience at a museum?</li>
-              <li>What keeps users engaged in a museum, which will help tailor the features of the app to best reflect this</li>
-              <li>Understanding what the best ways to display other people’s opinions are</li>
-              <li>Understanding best ways to summarize personal data from each visit</li>
+        <h3>Partner Requests</h3>
+        <p>Because this app's primary purpose is to collect research data from Hood Museum-goers, the FINN Lab partners presented to us their idea of the app's general flow that would best inform their research goals.</p>
+        <p>Add visual of this flow?</p>
+        <p>As such, we made sure to adhere to our partner's research goals and preferred flow during our ideation and design process.</p>
+      </section>
 
+      <section className="user-research">
+        <h3>User interviews</h3>
+        <p>We brainstormed interview questions and conducted interviews for students and faculty around campus that had varying degrees of interest in museum going and/or relevant backgrounds in art. The goal of our user interviews was to gauge the following:</p>
+        <ul>
+          <li>What is the typical user's experience at a museum?</li>
+          <li>What keeps users engaged in a museum, which will help tailor the features of the app to best reflect this</li>
+          <li>Understanding what the best ways to display other people’s opinions are</li>
+          <li>Understanding best ways to summarize personal data from each visit</li>
+        </ul>
+        <p>Thus, we generated 5 main categories of questions to ask our users, each with various subquestions:</p>
+        <ul>
+          <li>How do you currently interact with museums?</li>
+          <li>What attracts you to specific artworks/exhibits?</li>
+          <li>Do you seek out outside opinions of the things that you experience (art, movies, books, plays, TV shows, etc)?</li>
+          <li>Is there anything you find challenging or annoying about finding other opinions on art?</li>
+          <li>Do you have any apps that summarize your activity?</li>
+        </ul>
+      </section>
 
-              <p>Thus, we generated 5 main categories of questions to ask our users, each with various subquestions:</p>
-              <li>How do you currently interact with museums?</li>
-              <li>What attracts you to specific artworks/exhibits?</li>
-              <li>Do you seek out outside opinions of the things that you experience (art, movies, books, plays, TV shows, etc)?</li>
-              <li>Is there anything you find challenging or annoying about finding other opinions on art?</li>
-              <li>Do you have any apps that summarize your activity?</li>
-
-        </section>
-
+      <section className="data-analysis">
         <h3>Data analysis</h3>
-            <p>After our user interviews, we synthesized our findings into <strong>empathy maps</strong>. We organized our findings into what interviewees say, do, think, and feel. From our synthesis, we gathered these key insights:</p>
-
-            <img src="../assets/ambiguityinart/empathymaps.png" className="empathy-maps"/>
-
-            <li> <strong>Sharing with friends</strong> makes a user's experience more enjoyable, such as sharing insights across social media apps. One example is how Spotify stats show an “an emblem of you”. This feature can get more people to come to the museum</li>
-            <li> Summary stats more satisfying to receive all at end, higher anticipation, also more accurate depiction when more data to present vs. one at a time with minimal data. slideshow recap of what you viewed/your interpretations in addition to comparative stats</li>
-            <li> Users want to <strong>know more about the art they are looking at</strong> - they want to know the story behind the art, the artist, and the context in which the art was created</li>
-            <li>slideshow recap of what you viewed/your interpretations in addition to comparative stats</li>
-            <li>short blurbs more well liked but do we include blurbs or not? - does this sway views early on?</li>
-
+        <p>After our user interviews, we synthesized our findings into <strong>empathy maps</strong>. We organized our findings into what interviewees say, do, think, and feel. From our synthesis, we gathered these key insights:</p>
+        <img src="../assets/ambiguityinart/empathymaps.png" className="empathy-maps" alt="Empathy Maps"/>
+        <ul>
+          <li><strong>Sharing with friends</strong> makes a user's experience more enjoyable, such as sharing insights across social media apps. One example is how Spotify stats show an “emblem of you”. This feature can get more people to come to the museum</li>
+          <li>Summary stats are more satisfying to receive all at the end, higher anticipation, also more accurate depiction when more data is presented vs. one at a time with minimal data. Slideshow recap of what you viewed/your interpretations in addition to comparative stats</li>
+          <li>Users want to <strong>know more about the art they are looking at</strong> - they want to know the story behind the art, the artist, and the context in which the art was created</li>
+          <li>Slideshow recap of what you viewed/your interpretations in addition to comparative stats</li>
+          <li>Short blurbs are more well-liked, but do we include blurbs or not? - Does this sway views early on?</li>
+        </ul>
         <img src="assets/images/project1-research.jpg" alt="Research Image" className="case-study-image"/>
-
+      </section>
     </div>
   );
 }

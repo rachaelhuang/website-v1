3a4def1c68af4b1d27ecc68710397565e9e9cbc2
Rachael <156548776+rachaelhuang@users.noreply.github.com>
2024-08-22 00:33:48 -0400

    case study work

--- src/pages/ambiguityinart.js
+++ src/pages/ambiguityinart.js
@@ -29,27 +29,27 @@ function AmbiguityInArt() {
 
       <section className="information">
         <div className="info-section">
-          <p>Timeline</p>
-          <p>10 weeks (March-May 2024); development ongoing through summer 2024</p>
+          <h4>Timeline</h4>
+          <p>10 weeks (March-May 2024); development through summer 2024</p>
         </div>
         <div className="info-section">
-          <p>Role</p>
+          <h4>Role</h4>
           <p>Designer (UI/UX, graphics) </p>
         </div>
         <div className="info-section">
-          <p>Team</p>
+          <h4>Team</h4>
           <p>4 Designers</p>
           <p>4 Developers</p>
           <p>1 Project Manager</p>
         </div>
         <div className="info-section">
-          <p>Tools</p>
+          <h4>Tools</h4>
           <p>Figma</p>
           <p>Adobe Photoshop</p>
           <p>Adobe After Effects</p>
         </div>
         <div className="info-section">
-          <p>Skills</p>
+          <h4>Skills</h4>
           <p>Prototyping</p>
           <p>Visual Design</p>
           <p>Interaction Design</p>
@@ -65,7 +65,7 @@ function AmbiguityInArt() {
             They envisioned a mobile 'edutainment' experience where museum-goers could enjoy an <strong>interactive digital experience</strong> while simultaneously <strong>learning about artwork</strong>.
             Our team was tasked with designing and developing a mobile app that allows museum visitors to interpret Hood Museum artworks through Mad-Libs style inputs.</p>
 
-        <h3>Key Partners</h3>
+        <h4>Key Partners</h4>
         <p>Throughout the 10 week sprint, we met weekly with FINN Lab reps Professor Emily Finn and graduate student Clara Sava-Segal for reviews, standups, and feedback.</p>
       </section>
 
@@ -102,29 +102,51 @@ function AmbiguityInArt() {
       </section>
 
       <section className="partner-requests">
-        <h3>Partner Requests</h3>
-        <p>With the goal of best gathering their research data, the FINN Lab partners presented to us their idea of the app's general flow that would best inform their research goals.</p>
-        <p>Add visual of this flow?</p>
-        <p>We were working within a unique constraint; we needed to <strong> balance an enjoyable user experience alongside the requirements of a scientific study. </strong> 
-          For example, the partners required specific quantiative data that we needed our designs to reflect. As such, some screens needed to be separated and extra instructions included to ensure that participants used the app correctly.
-          Different age groups as well.
+        <h2>Partner Requests</h2>
+        <p>In order to properly gather their research data, the FINN Lab partners presented to us their idea of the flow that would best achieve their research goals.</p>
+        <p>Museum map selection -> Intial interpretation -> Alternative interpretation -> Re-evaulate interpretation -> Repeat -> Activity Summary</p>
+        <p>We were working within a unique constraint; we needed to <strong> balance an enjoyable user experience alongside the requirements and goals of a scientific study. </strong> 
+          For example, the partners needed to record specific quantitative data, but not make it obvious to the users, which we needed our designs to reflect. As such, some screens would need to be separated and extra instructions included to ensure that participants used the app correctly.
         </p>
-
-        <p>As such, we made sure to adhere to our partner's research goals and preferred flow during our ideation and design process.</p>
       </section>
 
       <section id="problem-statement" className="problem-statement">
+        <p>We asked ourselves:</p>
         <h2 style={{ fontStyle: 'italic', fontSize: '30px'}}>How might we...</h2>
         <p>facilitate a research study on how people interpret art while simultaneously improving their museum experience?</p>
+      </section>
 
-       
+      <section className="industry-research">
+        <h3>INDUSTRY RESEARCH</h3>
+        <h2>Analyzing existing apps</h2>
+        <p>Museum apps are a unique category of technology that none of us had frequently used before.
+          We had the broad user flow presented to us by the partners, but we also needed to see what kind of museum apps currently exist in the market.
+          As such, we conducted research on <strong>11</strong> mobile and web apps related to art museum experiences, and <strong>3</strong> mobile apps that have activity summary features.
+          </p>
+
+        <div style={{ backgroundColor: '#f0f0f0', padding: '1rem', display: 'inline-block', borderRadius: '15px' }}>
+          <img
+            src="../assets/ambiguityinart/industryresearchvisual.png"
+            className="case-study-image"
+          />
+        </div>
+        
+        <p> We noted the pros and cons for each app.</p>
+        <div style={{ backgroundColor: '#f0f0f0', padding: '1rem', display: 'inline-block', borderRadius: '15px' }}>
+          <img
+            src="../assets/ambiguityinart/industryresearch_visual2.png"
+            className="case-study-image"
+          />
+        </div>
+
       </section>
 
 
+    
       <section id='user-research' className="user-research">
-        <h3>User Research</h3>
-        <h2>User interviews</h2>
-        <p>In order to understand a typical visitor's experience in the Hood Museum, we conducted interviews of students and faculty across Dartmouth's campus. Each interviewee had a varying degree of interest in museums or a relevant background in museum art. The goal of our user interviews was to gauge the following:</p>
+        <h3>USER RESEARCH</h3>
+        <h2>User Interviews</h2>
+        <p>In order to understand the typical visitor's experience in the Hood Museum, we conducted interviews across multiple age groups. We sat down with students and faculty across Dartmouth's campus. Each interviewee had a varying degree of interest in museums or a relevant background in museum art. The goal of our user interviews was to gauge the following:</p>
         <div>
           <li>What is a typical visitor's experience at a museum, and what keeps them engaged?</li>
           <li>What is the most user-friendly way to display other people’s opinions?</li>
@@ -133,7 +155,7 @@ function AmbiguityInArt() {
       </section>
 
       <section className="data-analysis">
-        <h3>Data analysis</h3>
+        <h2>Data analysis</h2>
         <p>After our user interviews, we synthesized our findings into <strong>empathy maps</strong>. We organized our findings into what interviewees say, do, think, and feel. From our synthesis, we gathered these key insights:</p>
         <img src="../assets/ambiguityinart/empathymaps.png" className="empathy-maps" alt="Empathy Maps"/>
         <ul>
@@ -147,24 +169,25 @@ function AmbiguityInArt() {
       </section>
 
       <section class="ideation">
-        <h2>Ideation</h2>
-        <p>Once we wrapped up with interview findings and our feature spec, each of us created Crazy 8 sketches (link) as the initial ideation of screen designs.
+        <h3>IDEATION</h3>
+        <h2>Sketching the User Flow</h2>
+        <p>Once we wrapped up with interview findings and our feature spec, each designer created Crazy 8 sketches as the initial ideation of screen designs.
           I couldn't quite get to 8 on time, but I drew 6 screens, imagining the flow from the museum map to an individual artwork, then to interpretation of the artwork,
           and lastly the summary of the user's total session, reminiscent of a Spotify-Wrapped like summary. </p>
         <img src="../assets/ambiguityinart/crazy8s.png"/>
 
         <p>After Crazy 8s, we created solution sketches to further define our screens. Because the research study requires the consent of the users, we needed a clear presentation of the study's consent form before allowing the user to engage in the study itself. </p>
         <img src="../assets/ambiguityinart/sketch.png"/>
-      </section>
 
-      
-
-      <section id='design-process' class="design-process">
-        <h2>Design Process</h2>
         <p>We defined a wireflow to ensure that the layout of the screens were clear and easy to understand.</p>
         <img src="../assets/ambiguityinart/wireflow.png"/>
   
-        <h3>Grayscales</h3>
+      </section>
+
+      
+      <section id='design-process' class="design-process">
+        <h3>DESIGN</h3>
+        <h2>Grayscales</h2>
         <p>After the wireflow, each designer created our own grayscale screens. After initial iterations, we came together to discuss which designs had the best usability.</p>
         <p>After receiving user feedback from two rounds of iterations, we  </p>
 

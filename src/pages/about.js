394fe357221dde80b7ff4c32d21169c2381ae2d6
Rachael <156548776+rachaelhuang@users.noreply.github.com>
2024-08-29 16:52:03 -0400

    about me

--- src/pages/about.js
+++ src/pages/about.js
@@ -4,12 +4,17 @@ import './about.css';
 function About() {
   return (
     <div className="about">
+      <img src="/about-me-pic.png" className="about-image" alt="About Me" />
       <div className="about-text">
         <h4>About Me</h4>
         <h3 className="header">I'm Rachael. Thanks for stopping by!</h3>
-        <p>I’m a product designer and a sophomore at Dartmouth College, majoring in Computer Science and Digital Arts.
-          My journey began with a strong foundation in traditional art, which naturally evolved into a passion for graphic design as I became fascinated by the intersection of technology and creativity.</p>
-        <p>In college, I discovered product design, a field that perfectly merges my love for art and technology. With new avenues for me to channel my creativity while solving real-world problems, I’m eager to continue crafting intuitive and impactful user experiences that blend form and function.
+        <p>
+          I’m a sophomore at Dartmouth College, majoring in Computer Science and Digital Arts.
+          My journey began with a strong foundation in traditional art, which naturally evolved into a passion for graphic design as I became fascinated by the intersection of technology and creativity.
+        </p>
+        <p>
+          In college, I discovered product design, a field that perfectly merges my passions for art, design, and tech. With new avenues for me to channel my creativity while solving real-world problems,
+          I’m eager to continue crafting impactful user experiences that blend form and function.
         </p>
       </div>
     </div>

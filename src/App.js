f1720e4b719f45069ba4761695b9667cd2d6a47f
Rachael <156548776+rachaelhuang@users.noreply.github.com>
2024-08-30 01:54:41 -0400

    fixed graphics grid layout

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import "./App.css";
-import Navbar from './components/Navbar.js';
-import Footer from './components/Footer.js';
-import ScrollToTop from './components/ScrollToTop'; // Import ScrollToTop
+import Navbar from "./components/Navbar.js";
+import Footer from "./components/Footer.js";
+import ScrollToTop from "./components/ScrollToTop"; // Import ScrollToTop
 
-import Home from './pages/home.js';
-import Work from './pages/work.js';
-import Play from './pages/play.js';
-import About from './pages/about.js';
-import AmbiguityInArt from './pages/ambiguityinart.js';
-import Dineable from './pages/dineable.js';
+import Home from "./pages/home.js";
+import Work from "./pages/work.js";
+import Play from "./pages/play.js";
+import About from "./pages/about.js";
+import AmbiguityInArt from "./pages/ambiguityinart.js";
+import Dineable from "./pages/dineable.js";
 
 function App() {
   return (
     <div className="App">
       <Router>
-        <ScrollToTop /> 
+        <ScrollToTop />
         <Navbar />
         <Routes>
           <Route path="/" element={<Home />} />
